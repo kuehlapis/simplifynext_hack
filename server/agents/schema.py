db9288f6a46a72e5c098ef09fc5a6d37ec87f83f
yaosheng <yaosheng2001@gmail.com>
2025-08-21 22:16:16 +0800

    updated to langchain with structured output

--- server/agents/schema.py
+++ server/agents/schema.py
@@ -1,3 +1,9 @@
-# from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List
 
-# class IntakeAgentOutput(BaseModel):
+
+class IntakeAgentOutput(BaseModel):
+    title: str = Field(..., description="Title of the intake content")
+    clauses: List[str] = Field(
+        ..., description="Clauses associated with the intake content"
+    )
--- test.py
+++ test.py
@@ -5,24 +5,24 @@ import os
 import json
 
 
-file_name = os.path.abspath("messed_up_rental_agreement_long.pdf")
+file_name = os.path.abspath("server/contracts/messed_up_rental_agreement_long.pdf")
 with open( file_name, "rb") as f:
     pdf_bytes = f.read()
 
 markdown = OCRService.pdf_to_markdown(pdf_bytes, file_name)
 
-intake_agent = IntakeAgent(document=markdown)
-intake_agent.text_normalization()
+intake_agent = IntakeAgent()
+intake_agent.normalization(markdown)
 
-with open("intake_agent.json", "r", encoding="utf-8") as f:
-    intake_data = json.load(f)
+# with open("intake_agent.json", "r", encoding="utf-8") as f:
+#     intake_data = json.load(f)
 
-clauses_json = intake_data["anchor"]["content"]
+# clauses_json = intake_data["anchor"]["content"]
 
-analyser_agent = AnalyserAgent()
-analysis_result = analyser_agent.analyze(clauses_json)
+# analyser_agent = AnalyserAgent()
+# analysis_result = analyser_agent.analyze(clauses_json)
 
-print(json.dumps(analysis_result, indent=2, ensure_ascii=False))
+# print(json.dumps(analysis_result, indent=2, ensure_ascii=False))
 
-with open("analysis_result.json", "w", encoding="utf-8") as outfile:
-    json.dump(analysis_result, outfile, indent=2, ensure_ascii=False)
+# with open("analysis_result.json", "w", encoding="utf-8") as outfile:
+#     json.dump(analysis_result, outfile, indent=2, ensure_ascii=False)
